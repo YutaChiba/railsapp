401d886301f135dcbb934e78f24b43a60adb43f3
yuinchirn <l.serah.yuinch@gmail.com>
2014-07-05 17:06:22 +0000

    Ruby on Rails 4入門 #15

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,28 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+  
+  def new
+    @project = Project.new
+  end
     
-    
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+  
+  # パラメータのフィルタリング
+  private def project_params
+    # titleだけアクセス許可
+      params[:project].permit(:title)
+    end
+  
 end
